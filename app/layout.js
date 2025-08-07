b26735af1235d4240f62178e4ab98b90ed37ffd4
Digital Esposure Online Services <131973180+Deos2023@users.noreply.github.com>
2025-08-07 12:10:24 +0530

    add GTM

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import ScrollToTopButton from "./component/ScrollToTopButton";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,21 +17,44 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Home - Amantran Resorts | Luxury Stays Across Bengal’s Most Iconic Destinations",
-  description: "Experience soulful getaways with Amantran Resorts — premium hotels and retreats in Tarapith, Sundarban, Mandarmani, Darjeeling & more. Blending luxury, tradition, and serenity for unforgettable stays.",
+  description:
+    "Experience soulful getaways with Amantran Resorts — premium hotels and retreats in Tarapith, Sundarban, Mandarmani, Darjeeling & more. Blending luxury, tradition, and serenity for unforgettable stays.",
   icons: {
     icon: "/images/logo.png",
-  }
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Google Tag Manager script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PRH7SH58');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        {/* ✅ Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PRH7SH58"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
         <ScrollToTopButton />
       </body>
     </html>
